b8dbbbfef404f0c6a3ea16d81c42adb725f53edf
kakitaku00 <webtrakakinuma@gmail.com>
2019-07-12 14:42:51 +0900

    mymarkdonw add git

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <h1><router-link :to="{ name: 'top' }">{{ title }}</router-link></h1>
+    <nav class="nav">
+      <ul class="navList">
+        <li class="navListItem">ABOUT</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header",
+  props: ["title"],
+  data() {
+    return {
+      title: title
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    width: 100%;
+    padding: 25px 50px;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+  }
+</style>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="home">
+  <Header :title="msg"></Header>
+  <div class="container">
+    <section class="section">
+      <header>
+        <h2 class="section__title">SIMPLE Markdown Editor</h2>
+      </header>
+      <p class="section__text">最小限の機能に絞ったシンプルなエディターです</p>
+    </section>
+    <button class="loginBtn" @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+</div>
+</template>
+
+<script>
+import Header from "./Header"
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to MyMarkdown'
+    }
+  },
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  },
+  components: {
+    Header: Header
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .section {
+    &__title {
+      font-size: 24px;
+    }
+
+    &__text {
+      margin-top: 20px;
+    }
+  }
+
+  .loginBtn {
+    margin-top: 30px;
+  }
+</style>
