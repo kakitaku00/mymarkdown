b2c15266f4ae5d3484b0b8f0e2db7bbafe7ddffb
kakitaku00 <webtrakakinuma@gmail.com>
2019-07-18 10:19:33 +0900

    btn css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -46,6 +46,26 @@ export default {
   }
 
   .loginBtn {
+    cursor: pointer;
     margin-top: 30px;
+    height: 40px;
+    border-radius: 5px;
+    white-space: nowrap;
+    box-shadow: 1px 1px 0px 1px rgba(0,0,0,0.05);
+    border: 1px solid #ccc;
+    transition-property: background-color, box-shadow;
+    transition-duration: 150ms;
+    transition-timing-function: ease-in-out;
+
+    &:focus,
+    &:hover {
+      box-shadow: 1px 4px 5px 1px rgba(0,0,0,0.1);
+    }
+
+    &:active {
+      background-color: #e5e5e5;
+      box-shadow: none;
+      transition-duration: 10ms;
+    }
   }
 </style>
